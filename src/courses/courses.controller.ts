d2e32e5eea4a139f4c5478598fdab159a1c818d9
Patrick Simões <patricksimoes25@gmail.com>
2024-11-02 20:01:18 -0300

    Criação de Services para os Cursos

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -6,51 +6,49 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  Patch,
   Post,
-  Res,
+  Put,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
-  //Rota SEM parâmetro
+  constructor(readonly coursesService: CoursesService) {}
   @Get()
-  index() {
-    return 'Rota base';
-  }
-
-  //Rota com sub rota
-  @Get('lista')
-  findAll(@Res() response) {
-    // return response.status(201).send('Todos os cursos');
-    return response.status(201).json({ message: 'Todos os cursos' });
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   // Rota com um Parâmetro
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `Curso ID ${id}`;
+  findOne(@Param('id') id: number) {
+    return this.coursesService.findOne(id);
   }
 
   // @HttpCode(204) //Maneira de definir o retorno HTTP.
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
-  // Atualização de parte dos registros.
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    console.log(body);
-
-    return `Course updated for ID ${id}`;
+  // Atualização total do registro.
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.coursesService.update(id, body);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  destroy(@Param(':id') id: string) {
-    return `Delete course with ID ${id}`;
+  destroy(@Param(':id') id: number) {
+    return this.coursesService.destroy(id);
   }
+
+  //Rota com sub rota
+  // @Get('lista')
+  // findAll(@Res() response) {
+  //   // return response.status(201).send('Todos os cursos');
+  //   return response.status(201).json({ message: 'Todos os cursos' });
+  // }
 
   // Rota com Multiplos Parâmetros
   // @Get(':id/:name')
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,83 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from 'src/courses/courses.entity';
+
+@Injectable()
+export class CoursesService {
+  courses: Course[] = [
+    {
+      id: 1,
+      name: 'Introdução ao TypeScript',
+      description:
+        'Aprenda os fundamentos do TypeScript, uma linguagem que expande o JavaScript.',
+      tags: ['TypeScript', 'Programação', 'JavaScript'],
+    },
+    {
+      id: 2,
+      name: 'Desenvolvimento Web com React',
+      description:
+        'Curso completo para criar aplicações web interativas com React.',
+      tags: ['React', 'Desenvolvimento Web', 'Frontend', 'JavaScript'],
+    },
+    {
+      id: 3,
+      name: 'Ciência de Dados com Python',
+      description:
+        'Domine análise de dados e aprendizado de máquina com Python.',
+      tags: [
+        'Python',
+        'Ciência de Dados',
+        'Machine Learning',
+        'Análise de Dados',
+      ],
+    },
+    {
+      id: 4,
+      name: 'Fundamentos de Cibersegurança',
+      description:
+        'Conheça os princípios básicos de segurança da informação e redes.',
+      tags: ['Cibersegurança', 'Segurança', 'Redes', 'Gestão de Riscos'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    const course = this.courses.find((el) => Number(el.id) == id);
+    if (!course) {
+      throw new HttpException(`Curso com o ID ${id} não foi encontrado`, HttpStatus.NOT_FOUND);
+    }
+
+    return course;
+  }
+
+  create(any): Course {
+    const newCourse: Course = any;
+    this.courses.push(newCourse);
+    return newCourse;
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const findCourse = this.courses.find((el) => el.id == id);
+
+    if (!findCourse) return 'Curso não encontrado';
+
+    const index = this.courses.findIndex((course) => course.id == id);
+
+    this.courses[index] = {
+      id,
+      ...updateCourseDTO,
+    };
+  }
+
+  destroy(id: Number) {
+    const index = this.courses.findIndex((course) => course.id == id);
+    
+    if (index === -1) return 'Curso não encontrado';
+
+    this.courses.splice(index, 1);
+
+    return this.findAll();
+  }
+}
